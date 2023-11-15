be1e9c45c38782474f4b55b4504afbd2f214d1b3
ertyr <91670210+ertyr@users.noreply.github.com>
2023-11-15 13:58:27 +0100

    new weights

--- track/track.py
+++ track/track.py
@@ -9,8 +9,9 @@ def runTrack(option, pathToFile=None):
     else: # option == 1:
         results = model.track(source=0, show=True, tracker="bytetrack.yaml")
 
-# loading pre-trained model saved at pt
-model = YOLO('yolov8n.pt')
 
-runTrack(1, "track/fish.mov")
+# loading pre-trained model saved at pt
+model = YOLO('track/Weights/last.pt')
+
+runTrack(0)
 
