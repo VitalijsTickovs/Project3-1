b050fee1211127fdb017540655e31f27e9a7c2a1
ertyr <91670210+ertyr@users.noreply.github.com>
2023-11-22 10:28:42 +0100

    progress

--- baselines/autoenc_basic/main.py
+++ baselines/autoenc_basic/main.py
@@ -1,2 +1,46 @@
 import torch
+from torch import nn
+from torch.utils.data import DataLoader
 
+# Layer structure:
+#   in -> encoder_1 -> encoder_2 -> code -> decoder_1 -> decoder_2 -> out
+# - 2 layers for decoding and encoding
+
+## METHODS & CLASSES
+# TODO: Site pytorch documentation
+class ED_Network(nn.Module): # inherit from nn.Module
+    def __init__(self): # initialise layers
+        super().__init__()
+        self.flatten = nn.Flatten() # flatten variable of self
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(4*15, 50), # 4 features * 15 different time points
+            nn.ReLU(), # keep ReLU for now (lower computation) althoguh tempted to add LeakyReLU (dying neuron problem resolved)
+            nn.Linear(50, 40), # encoder_1 -> encoder_2
+            nn.ReLU(),
+            nn.Linear(40, 30), # encoder_2 -> code
+            nn.ReLU(),
+            nn.Linear(30, 40), # code -> decoder_1
+            nn.ReLU(),
+            nn.Linear(40, 50), # decoder_1 -> decoder_2
+            nn.ReLU(),
+            nn.Linear(50, 60), # decoder_2 -> output
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+## MAIN
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+model = ED_Network()
--- baselines/autoenc_basic/plan&log.md
+++ baselines/autoenc_basic/plan&log.md
@@ -16,3 +16,12 @@
 ### Log
 ##### 19 Nov 2023
 - Decided to use PyTorch instead of TensorFlow because is supposedly easier to debug and less steep learning curve
+
+##### 22 Nov 2023
+- May actually be possible to use intel extnesion (simple changes to the optimizer type in code the rest of the changes remain the same)
+    - Nevermind it is for linux and windows OS
+    - No options, left with CPUs
+- Recomended 2 layers for encoders and decoders
+- Multiple inputs only needed when multiple different data sources (e.g. visual and audio)
+    - Solution 1: multiple input layers + pooling
+    - Solution 2: flatten + CNN
