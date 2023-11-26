ca8db3026d2da564f5b5cb1b5058ddcc7acbb495
ertyr <91670210+ertyr@users.noreply.github.com>
2023-11-26 10:01:25 +0100

    Constructing full data processing file

--- baselines/autoenc_basic/dataset.py
+++ baselines/autoenc_basic/dataset.py
@@ -1,4 +1,8 @@
 import pandas as pd
+# copy of file from visual.ipynb
+# objective to create the arrays of data needed to train the network:
+#   - X/ features
+#   - Y/ outputs
 
 ### METHODS:
 
@@ -6,7 +10,14 @@ import pandas as pd
 
 ### MAIN:
 
-contents = pd.read_json("Data/SkeletonData/testFile.json")
-print(type(contents))
-print()
-print(contents)
+# Read file. TODO: multiple names
+trainNames = ["skCrateLeft1.json", "skCrateLeft2.json", "skCrateLeft3.json", 
+              "skCrateRight1.json", "skCrateRight2.json", "skCrateRight3.json", 
+              "skCupLeft1.json", "skCupLeft2.json", "skCupLeft3.json",
+              "skCupRight1.json", "skCupRight2.json", "skCupRight3.json", 
+              "skFeederLeft1.json", "skFeederLeft2.json", "skFeederLeft3.json",
+              "skFeederRight1.json", "skFeederRight2.json", "skFeederRight3.json"]
+rlPth = "Data/SkeletonData/"
+for name in trainNames:
+    fullPath = rlPth+name
+    contents = pd.read_json(fullPath)
