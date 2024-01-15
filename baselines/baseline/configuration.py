8241818c60a8b8ee72c01f2416eaf7b9d8cd7e7a
ertyr <91670210+ertyr@users.noreply.github.com>
2024-01-15 11:22:48 +0100

    Clean up unsused code and comments

--- baselines/baseline/configuration.py
+++ baselines/baseline/configuration.py
@@ -15,12 +15,6 @@ def initGraph(input):
         for objTup in pos:
             # get reference for this node
             ref = objTup[0]+str(objTup[1][0])+str(objTup[1][1])+str(objTup[1][2])+"_"+str(prev_ref)
-            # foundEqls = False
-            # for n in G.nodes:
-            #     if checkEq(n, ref):
-            #         G.add_edge(prev_ref, n)
-            #         foundEqls = True
-            #         prev_ref = n
             edges_to_add = []
             foundEqls = False
             for n in G.nodes:
@@ -50,57 +44,6 @@ def checkEq(nd1, nd2):
     splt2 = set(nd2.split("_"))
 
     return (nd1!=nd2) and (splt1 == splt2)
-
-class Tree:
-    def __init__(self, input):
-        self.root = TreeNode(None, None)
-        self.levels = [[]]*len(input+1)
-
-        # append root to queue
-        allPoss = list(itertools.permutations(input))
-        self.levels[0].append(self.root)
-
-        for pos in allPoss: # iterate through possibilities
-            previousNode = self.root
-            lvl = 1
-            for objTup in pos:
-                newNode = TreeNode(objTup, previousNode)
-                self.levels[lvl].append(newNode)
-                previousNode = newNode
-
-
-class TreeNode:
-    """A basic tree node class."""
-    def __init__(self, tuple_in, parent): # for standard node
-        if (parent!= None):
-            self.name = tuple_in[0]
-            self.pos = tuple_in[1] #tuple
-            self.size = tuple_in[2] #tuple
-        else:
-            self.name = None
-            self.pos = None
-            self.size = None
-        self.prob = 0
-        self.children = []
-
-        self.parent = parent # if 'None' then root
-
-    def add_child(self, child_node):
-        """Adds a child to this node."""
-        self.children.append(child_node)
-
-    def remove_child(self, child_node):
-        """Removes a child from this node."""
-        self.children = [child for child in self.children if child != child_node]
-
-    def traverse(self):
-        """Traverses the tree starting from this node."""
-        nodes = [self]
-        while nodes:
-            current_node = nodes.pop()
-            print(current_node.value)
-            nodes.extend(current_node.children)
-
 
 if __name__ == "__main__":
     configuration = [("Cup", (0,0,0), (1,1)), 
